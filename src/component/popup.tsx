264c2df4f58971c97c737903683aeecc54a5835f
kinoshita197083 <83145802+kinoshita197083@users.noreply.github.com>
2023-07-18 21:19:22 +1000

    icon showcase container

--- src/component/popup.tsx
+++ src/component/popup.tsx
@@ -37,12 +37,15 @@ const Popup = (props: PopupProps) => {
             className={[state ? 'transition-transform fixed rounded-[35px] blur-dark inset-0 bg-black lg:w-[25rem] md:w-[25rem] w-[80%] lg:h-[30rem] md:h-[28rem] h-[25rem] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] lg:p-[1%] md:p-[2%] p-[4%] duration-500 ease-in-out' : 'fixed top-[300%] left-[50%] translate-x-[-50%] w-[25rem] h-[30rem] transition-all opacity-0 duration-1000'].join(' ')}
         >
             {children}
-            <h2 className='text-gray-100 mx-auto text-center mt-[5%] text-[1.2rem]'>
-                {textContent}
-            </h2>
+            <div role='prompt-container' className='lg:h-[15%] h-[25%] overflow-auto'>
+                <h2 className='text-gray-100 mx-auto text-center mt-[5%] md:mt-[2%] text-[1.2rem]'>
+                    {textContent}
+                </h2>
+            </div>
+
             <span
                 onClick={() => setState(false)}
-                className="absolute aspect-square bg-red-500 lg:w-[1.5rem] md:w-[1.5rem] w-[2rem] rounded-full text-transparent hover:text-gray-700 flex justify-center items-center cursor-pointer bottom-[6%] right-[8%] lg:top-[6%] lg:left-[6%] md:top-[6%] md:left-[6%]">
+                className="absolute aspect-square bg-red-500 opacity-[75%] lg:w-[1.5rem] md:w-[1.5rem] w-[2rem] rounded-full text-transparent hover:text-gray-700 flex justify-center items-center cursor-pointer bottom-[6%] right-[8%] lg:top-[6%] lg:left-[6%] md:top-[6%] md:left-[6%]">
                 <FontAwesomeIcon icon={faXmark} />
             </span>
         </div>
